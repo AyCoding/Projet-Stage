7ea68bbd445580fb1f069b8c0be0afb344a19413
Ertinox-08 <80890993+Ertinox-08@users.noreply.github.com>
2022-02-15 20:08:08 +0100

    Update

--- src/js/app.js
+++ src/js/app.js
@@ -1,48 +1,42 @@
 const task_title = document.querySelectorAll('h2');
 const task_list = document.querySelectorAll('section');
 
-const title = document.getElementsByClassName('title__task');
-const title_wrapper = document.getElementsByClassName('title__wrapper');
+// Pour ouvrir les contenus des tâches
+function toggleTask () {
+    
+    const title = document.querySelectorAll('.title__task');
+    const title_wrapper = document.querySelectorAll('.title__wrapper');
 
-// 
-// Si title == 1 alors ajout class ".active" à ".title__wrapper"
-// Sinon supprime .active" à ".title__wrapper"  
-// 
-
-for (let a = 0; a < title.length; a++) {
-
-    title[a].addEventListener("click", () => {
-
-        if (a == a) {
-            title_wrapper[a].classList.add('active');              
-        }
-
-    })
-        
+    for (let a = 0; a < title.length; a++) {
+        title[a].addEventListener('click', () => {
+            title_wrapper[a].classList.toggle('active');
+        })
+    }
+    
 }
-        
-       
+toggleTask();
 
-// console.log(title)
-
+// Ajout de la class "active" sur les sections en passant par "Projets en cours" ou "Projets finis" 
 for (let i = 0; i < task_title.length; i++) {
-
 
     task_title[i].addEventListener("click", () => {
 
-        
         if (i == 0) {
+
             task_title[0].classList.add('active');
             task_title[1].classList.remove('active');
             task_list[0].classList.add('active');
             task_list[1].classList.remove('active');
-            
+
         } else {
+
             task_title[1].classList.add('active');
             task_title[0].classList.remove('active');
             task_list[1].classList.add('active');
             task_list[0].classList.remove('active');
+
         }
 
     });
+
 }
