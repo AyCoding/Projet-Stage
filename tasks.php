f79a7b8ade3d291ae017ed8fd03830e56362df74
AyCoding <aymericdelange80@gmail.com>
2022-02-24 16:09:00 +0100

    Update

--- tasks.php
+++ tasks.php
@@ -24,36 +24,7 @@ forcer_utilisateur_connecte();
                 <section class="active">
                     <div class="add__project">
                         <h2>Ajout d'un projet</h2>
-                    </div>
-                    <div class="title">
-                        <h3 class="title__task">Installation raccordements eau - Commune Neufmanil
-                        </h3>
-
-                        <div class="title__wrapper">
-                            <h4>Section A1</h4>
-                            <h4>Section B5</h4>
-                            <div class="task__adds">
-                                <h4>Ajouter tâches</h4>
-                            </div>
-                        </div>
-
-                        <?php include("php/progress-line.php"); ?>
                         
-                    </div>
-                    <div class="title">
-                        <h3 class="title__task">Installation raccordements eau - Commune Aiglemont</h3>
-
-                        <div class="title__wrapper">
-                            <h4>Section A1</h4>
-                            <h4>Section B5</h4>
-                            <div class="task__adds">
-                                <h4>Ajouter tâches</h4>
-                            </div>
-                        </div>
-
-                        <?php include("php/progress-line.php"); ?>
-
-
                     </div>
                         <?php include("php/test.php") ?>
                 </section>
