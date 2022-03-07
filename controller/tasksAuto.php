5343dcdde54b65c05a3beb1b738fe05d14dee5ec
AyCoding <aymericdelange80@gmail.com>
2022-03-07 11:57:03 +0100

    Update

--- controller/tasksAuto.php
+++ controller/tasksAuto.php
@@ -13,7 +13,11 @@ $data = $result->fetchAll();
         <div class="title">
             <h3 class="title__task">
                     <?= $value['title']; ?>
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M15.728 9.686l-1.414-1.414L5 17.586V19h1.414l9.314-9.314zm1.414-1.414l1.414-1.414-1.414-1.414-1.414 1.414 1.414 1.414zM7.242 21H3v-4.243L16.435 3.322a1 1 0 0 1 1.414 0l2.829 2.829a1 1 0 0 1 0 1.414L7.243 21z"/></svg>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 13.172l4.95-4.95 1.414 1.414L12 16 5.636 9.636 7.05 8.222z" fill="rgba(51,51,51,1)"/></svg>
+                </div>
+
             </h3>
             <div class="title__wrapper">
                 <form action="" method="post">
